d0d26f9a89b8fb208f2658a2e3677a7575064032
francofallatti <francofallatti@gmail.com>
2022-06-28 03:54:22 -0300

    operaciones y print error

--- TPFinal.s
+++ TPFinal.s
@@ -2,6 +2,7 @@
   input_usuario: .asciz "                            "
   long_input= . - input_usuario
   mensaje_error: "Lo siento, mis respuestas son limitadas \n"
+  long_error= . - mensaje_error
   text_result: "##########"
   operacion: .byte 0
   num1: .int 0
@@ -9,8 +10,8 @@
   resultado: .int 0
   resto: .int 0
   mensaje_despedida: "Adios! \n"
+
 .text
-.global main
 leer_input_usuario:
  .fnstart
    mov r7, #3 /*ingreso standard output*/
@@ -18,15 +19,59 @@ leer_input_usuario:
    ldr r1, =input_usuario /*r0 = puntero de input*/
    ldr r2, =long_input
    ldr r2, [r2] /*r2 = cantidad de caracteres*/
-   swi 0 
+   swi 0
    bx lr
  .fnend
 es_numero:
  .fnstart
    ldrb r1, r3, #1 /*r1 = almacena el valor del caracter, r3 = puntero a input*/
-   cmp r1, #30 
+   cmp r1, #0x30 
    bhe posibleNum
-   cmp r1, #2D /*compara si es negativo, #2D = '-'*/
+   cmp r1, #0x2D /*compara si es negativo, #2D = '-'*/
    beq negativo
  .fnend
 
+.resolver_operacion:
+ .fnstart
+   push {lr}
+   ldrb r0,=operacion
+   ldrb r0,[r0]
+   ldr  r1,=num1  /*r0=operando de turno y lo compara con los ascii*/
+   ldr  r1,[r1]   /*salta a la operacion a realizar si es igual o a error*/
+   ldr  r2,=num2  /*r1 y r2 son los numeros*/
+   ldr  r2,[r2]
+   cmp r1,#0x2b
+   beq suma
+   cmp r1,#0x2d
+   beq resta
+   cmp r1,#0x2a
+   beq multiplicacion
+   cmp r1,#0x2f
+   beq division
+   bne mensaje_error
+   pop {lr}
+ .fnend
+.suma:
+.resta:
+.multiplicacion:
+.division:
+
+
+
+.print_mensaje_error:/*print mensaje de error*/
+ .fnstart
+   push {lr}
+   ldrb r1,=mensaje_error
+   ldr  r2,=long_error
+   mov r7,#4
+   mov r0,#1
+   swi 0
+   bx lr
+   pop {lr}
+ .fnend
+.global main
+main:
+	bl mensaje_error
+fin:
+   mov r7,#1
+   swi 0
